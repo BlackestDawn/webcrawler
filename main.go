20b47ecf9ea8d9836eae28aa570bd1a94c514691
Alexander Stauch <blacke4dawn@gmail.com>
2024-12-24 09:34:10 +0100

    lesson 5

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	argNum := len(os.Args)
+	if argNum < 2 {
+		fmt.Println("no website provided")
+		os.Exit(1)
+	} else if argNum > 2 {
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	}
+
+	baseUrl := os.Args[1]
+	fmt.Println("starting crawl of:", baseUrl)
 }
